7c534ec22d1a88c7e3322b68a61dad6e3ce210ca
Gabriel Fiocchi <gab@thinkapp.studio>
2020-07-08 02:00:35 +0200

    Fix route load.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '/',
+    path: '',
     pathMatch: 'full',
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  }
+  },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
